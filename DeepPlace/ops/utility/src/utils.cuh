fa0b9f611efd4b39ee945cdfbe7df502e542ff96
Rogerity <58288642+Rogerity@users.noreply.github.com>
2022-12-08 12:36:06 +0800

    Add files via upload

---
+++ DeepPlace/ops/utility/src/utils.cuh
@@ -0,0 +1,128 @@
+/**
+ * @file   utils.cuh
+ * @author Yibo Lin
+ * @date   Jan 2019
+ */
+#ifndef _DREAMPLACE_UTILITY_UTILS_CUH
+#define _DREAMPLACE_UTILITY_UTILS_CUH
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+// namespace
+#include "utility/src/namespace.h"
+// macro definitions
+#include "utility/src/defs.h"
+// print utilities
+#include "utility/src/msg.h"
+#include "utility/src/print.cuh"
+// math utilities
+#include "utility/src/math.h"
+// timer utilities
+#include "utility/src/timer.cuh"
+#include "utility/src/timer.h"
+// numeric limits
+#include "utility/src/limits.h"
+// simple data structures
+#include "utility/src/box.h"
+#include "utility/src/complex_number.h"
+#include "utility/src/diamond_search.h"
+#include "utility/src/flat_nested_vector.cuh"
+#include "utility/src/pitch_nested_vector.cuh"
+
+// placement database
+//#include "utility/src/detailed_place_db.h"
+//#include "utility/src/legalization_db.h"
+//#include "utility/src/make_placedb.h"
+
+DREAMPLACE_BEGIN_NAMESPACE
+
+template <typename T>
+__global__ void iota(T* a, int n) {
+  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n;
+       i += blockDim.x * gridDim.x) {
+    a[i] = i;
+  }
+}
+
+template <typename T>
+__global__ void fill_array_kernel(T* array, int n, T v) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n) {
+    array[i] = v;
+  }
+}
+
+template <typename T>
+inline void fill_array(T* array, int n, T v) {
+  fill_array_kernel<<<ceilDiv(n, 512), 512>>>(array, n, v);
+}
+
+template <typename T>
+__global__ void reset_element_set_sizes_kernel(int num_sets,
+                                               T* element_set_sizes) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < num_sets) {
+    element_set_sizes[i] = 0;
+  }
+}
+
+template <typename T>
+__global__ void collect_element_sets_kernel(int n, int num_sets,
+                                            int max_set_size, const T* elements,
+                                            const int* element2partition_map,
+                                            T* element_sets,
+                                            int* element_set_sizes) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n) {
+    const T& element = elements[i];
+    int partition_id = element2partition_map[i];
+    assert(partition_id < num_sets);
+    int& size = element_set_sizes[partition_id];
+    int index = atomicAdd(&size, 1);
+    if (index < max_set_size) {
+      element_sets[partition_id * max_set_size + index] = element;
+    }
+  }
+}
+
+template <typename T>
+__global__ void correct_element_set_sizes_kernel(int num_sets, T max_set_size,
+                                                 T* element_set_sizes) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < num_sets) {
+    T& size = element_set_sizes[i];
+    size = min(size, max_set_size);
+  }
+}
+
+/// @brief gather elements into sets according to element2partition_map
+/// For example, elements = {a0, a1, a2, a3, a4, a5}
+/// element2partition_map = {1, 0, 0, 1, 2, 1}
+/// expected result element_sets = {{a1, a2, null}, {a0, a3, a5}, {a4, null,
+/// null}} Current implementation is not deterministic. If introducing sorting,
+/// determinism is possible.
+/// @param n length of elements
+/// @param num_sets number of partitions
+/// @param max_set_size maximum number of elements in a partition
+/// @param elements array of elements
+/// @param element2partition_map map element index to partition
+/// @param element_sets output element sets in dimension num_sets x max_set_size
+/// @param element_set_sizes size of each set in dimension num_sets x 1
+template <typename T>
+inline __host__ void gather(int n, int num_sets, int max_set_size,
+                            const T* elements, const int* element2partition_map,
+                            T* element_sets, int* element_set_sizes) {
+  fill_array(element_sets, num_sets * max_set_size,
+             std::numeric_limits<T>::max());
+  reset_element_set_sizes_kernel<<<ceilDiv(num_sets, 512), 512>>>(
+      num_sets, element_set_sizes);
+  collect_element_sets_kernel<<<ceilDiv(n, 512), 512>>>(
+      n, num_sets, max_set_size, elements, element2partition_map, element_sets,
+      element_set_sizes);
+  correct_element_set_sizes_kernel<<<ceilDiv(num_sets, 512), 512>>>(
+      num_sets, max_set_size, element_set_sizes);
+}
+
+DREAMPLACE_END_NAMESPACE
+
+#endif
---
+++ DeepPlace/ops/weighted_average_wirelength/src/weighted_average_wirelength_cuda_kernel.cu
@@ -0,0 +1,123 @@
+#include <cfloat>
+#include <stdio.h>
+#include "assert.h"
+#include "cuda_runtime.h"
+#include "utility/src/utils.cuh"
+#include "weighted_average_wirelength/src/functional_cuda.h"
+
+DREAMPLACE_BEGIN_NAMESPACE
+
+template <typename T, typename V>
+int computeWeightedAverageWirelengthCudaLauncher(
+    const T *x, const T *y,
+    const int *pin2net_map,
+    const int *flat_netpin,
+    const int *netpin_start,
+    const unsigned char *net_mask,
+    int num_nets,
+    int num_pins,
+    const T *inv_gamma,
+    T *exp_xy, T *exp_nxy,
+    T *exp_xy_sum, T *exp_nxy_sum,
+    T *xyexp_xy_sum, T *xyexp_nxy_sum,
+    V *xy_max, V *xy_min,
+    T *partial_wl,
+    const T *grad_tensor,
+    T *grad_x_tensor, T *grad_y_tensor)
+{
+    int thread_count = 64;
+    int block_count_pins = (num_pins - 1 + thread_count) / thread_count;
+    int block_count_nets = (num_nets - 1 + thread_count) / thread_count;
+    dim3 block_size(thread_count, 2, 1);
+
+    if (grad_tensor)
+    {
+        // computeWeightedAverageWirelengthGradInterleaveNetByNet<<<block_count_pins, block_size>>>(
+        computeWeightedAverageWirelengthGradNetByNet<<<block_count_pins, thread_count>>>(
+            x, y,
+            exp_xy, exp_nxy,
+            exp_xy_sum, exp_nxy_sum,
+            xyexp_xy_sum, xyexp_nxy_sum,
+            flat_netpin,
+            netpin_start,
+            net_mask,
+            num_nets,
+            num_pins,
+            inv_gamma,
+            grad_tensor,
+            grad_x_tensor, grad_y_tensor);
+    }
+    else
+    {   
+        // compute max and min in one kernel (net by net)
+        // computeMaxMinInterleaveNetByNet<<<block_count_nets, block_size>>>(
+        computeMaxMinNetByNet<<<block_count_nets, thread_count>>>(
+            x, y,
+            flat_netpin,
+            netpin_start,
+            net_mask,
+            num_nets,
+            xy_max,
+            xy_min);
+        
+        // compute plus-minus exp, sum of plus-minus exp, sum of x*exp in one CUDA kernels (net by net)
+        // corresponding to the plus and minus a b c kernels in the DREAMPlace paper
+        // compute partial wirelength at the same time
+        computeABCKernelsInterleaveAndWLNetByNet<<<block_count_nets, block_size>>>(
+        // computeABCKernelsAndWLNetByNet<<<block_count_nets, thread_count>>>(
+            x,
+            flat_netpin,
+            netpin_start,
+            net_mask,
+            num_nets,
+            num_pins,
+            inv_gamma,
+            xy_max, xy_min,
+            exp_xy, exp_nxy,
+            exp_xy_sum, exp_nxy_sum,
+            xyexp_xy_sum, xyexp_nxy_sum,
+            partial_wl);
+    }
+
+    return 0;
+}
+
+#define REGISTER_KERNEL_LAUNCHER(T, V)                       \
+    int instantiateComputeWeightedAverageWirelengthLauncher( \
+        const T *x, const T *y,                              \
+        const int *pin2net_map,                              \
+        const int *flat_netpin,                              \
+        const int *netpin_start,                             \
+        const unsigned char *net_mask,                       \
+        int num_nets,                                        \
+        int num_pins,                                        \
+        const T *inv_gamma,                                  \
+        T *exp_xy, T *exp_nxy,                               \
+        T *exp_xy_sum, T *exp_nxy_sum,                       \
+        T *xyexp_xy_sum, T *xyexp_nxy_sum,                   \
+        V *xy_max, V *xy_min,                                \
+        T *partial_wl,                                       \
+        const T *grad_tensor,                                \
+        T *grad_x_tensor, T *grad_y_tensor)                  \
+    {                                                        \
+        return computeWeightedAverageWirelengthCudaLauncher( \
+            x, y,                                            \
+            pin2net_map,                                     \
+            flat_netpin,                                     \
+            netpin_start,                                    \
+            net_mask,                                        \
+            num_nets,                                        \
+            num_pins,                                        \
+            inv_gamma,                                       \
+            exp_xy, exp_nxy,                                 \
+            exp_xy_sum, exp_nxy_sum,                         \
+            xyexp_xy_sum, xyexp_nxy_sum,                     \
+            xy_max, xy_min,                                  \
+            partial_wl,                                      \
+            grad_tensor,                                     \
+            grad_x_tensor, grad_y_tensor);                   \
+    }
+REGISTER_KERNEL_LAUNCHER(float, int);
+REGISTER_KERNEL_LAUNCHER(double, int);
+
+DREAMPLACE_END_NAMESPACE
